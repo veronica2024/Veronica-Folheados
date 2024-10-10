fca75404b1d27e4981fbb5be56d79fe0a66b2ea0
veronica2024 <vn710949@gmail.com>
2024-10-10 07:26:57 -0300

    modificado

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-
-const dotenv = require('dotenv'); // Importa o pacote dotenv para gerenciar variáveis de ambiente
-
-//Configurar as Variáveis de ambiente
-
-dotenv.config(); // Carrega as variáveis definidas no arquivo '.env' para process.env(processos)
-
-//Importar as Bibliotecas
-const express = require('express'); // Importa o framework Express
-const cors = require('cors'); // Importa o pacote cors para permitir requisições de diferentes origens
-const bodyParser = require('body-parser'); // Importa o pacote body-parser para analisar o corpo das requisições HTTP
+//importar as bibliotecas 
 
 const db = require('./config/db'); // Importa a conexão com o banco de dados
-const vendasRoutes = require('./routes/vendas');
-// Importar as rotas de transações e autenticação 
-
-const authRoutes = require('./routes/auth'); // Importa as rotas de autenticação
 
 
-//inicializar nova aplicação Express
+const express = require('express'); // Importa o framework Express 
+const dotenv = require('dotenv'); // Importa o pacote dotenv para gerenciar variáveis de ambiente 
+const cors = require('cors'); // Importa o pacote cors para permi r requisições de diferentes origens 
+const bodyParser = require('body-parser'); // Importa o pacote body-parser para analisar o corpo das requisições HTTP
 
+
+//configurar as variáveis de ambiente 
+dotenv.config(); // Carrega as variáveis definidas no arquivo .env para process.env
+
+//Inicializa uma nova aplicação Express 
 const app = express(); // Inicializa uma nova aplicação Express
 
 
-//configurar o CORS e o bady-Parse
-
-app.use(cors()); // Habilita o CORS para todas as rotas
-app.use(bodyParser.json()); // Configura o body-parser para analisar requisições JSON
-
-
-// Usar as rotas de transações para todas as requisições que começam com /api/transac ons 
-app.use('/veronicafolheados/vendas', vendasRoutes); 
+//Configura o CORS e o body-parser 
+app.use(cors()); // Habilita o CORS para todas as rotas 
+app.use(bodyParser.json()); // Configura o body-parser para analisar requisições JSON 
 
 
-app.use('/veronicafolheados/auth', authRoutes);
-
-
-//Rota inicial para testar o servidor
-
-app.get('/', (req, res) => {
-    res.send(`Servidor está rodando na porta ${PORT}`); // Define uma rota inicial para testar o servidor
+//Rota inicial para testar o servidor 
+app.get('/', (req, res) => { 
+  res.send('Servidor está rodando'); // Define uma rota inicial para testar o servidor 
   });
 
-//Configurar o servidor para uma porta específica
 
-const PORT = process.env.PORT || 3000; // Define a porta a partir da variável de ambiente ou usa a porta 3000 como padrão
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`); // Loga uma mensagem informando que o servidor está rodando
-});
+// Configura o servidor para escutar em uma porta específica 
+const PORT = process.env.PORT || 3000; // Define a porta a par r da variável de ambiente ou usa a porta 3000 como padrão 
+app.listen(PORT, () => { 
+console.log(`Servidor rodando na porta ${PORT}`);  
+}); // Escreve uma mensagem informando que o servidor está rodando
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
